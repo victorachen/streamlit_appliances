059eb37e34e04afd627c99bd3c8f3c5040efb73b
Victor <vchen2120@gmail.com>
2022-10-12 09:45:10 -0700

    prbly not going to work

--- streamlit_appliances.py
+++ streamlit_appliances.py
@@ -1,1 +1,21 @@
 print('whatever')
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+db = firestore.client()
+
+entire_collection = db.collection('Vacancy').get()
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(st.secrets["firestore_keys_baby"])
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+entire_collection = db.collection('Appliances').get()
+for doc in entire_collection:
+    d = doc.to_dict()
+    st.write(d)
