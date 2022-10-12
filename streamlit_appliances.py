33eee7034625f1d105ef55a2796b4cbf69bee7fa
Victor <vchen2120@gmail.com>
2022-10-12 12:28:34 -0700

    maybe work this time

--- streamlit_appliances.py
+++ streamlit_appliances.py
@@ -8,11 +8,28 @@ if not firebase_admin._apps:
     cred = credentials.Certificate(st.secrets["firestore_keys_baby"])
     firebase_admin.initialize_app(cred)
 
+st.set_page_config(
+    page_title='Vacancies!',
+    layout="wide",
+    initial_sidebar_state="expanded",
+    )
+
+items = ['Washer_(Side_by_Side)','Dryer_(Side_by_Side)','Washer_&_Dryer_(Stackable)','Fridge','Stove','Outside_Cleanup','ReGlazing','Stairs','Awning','AC_unit','Roof_Foaming','Granite_Countertops']
+
 db = firestore.client()
 entire_collection = db.collection('Appliances').get()
 
-for doc in entire_collection:
-    d = doc.to_dict()
-    for entry in d:
-        line = entry + ': ' + d[entry]
-        st.write(line)
+st.write('Below is shit we need to do')
+col1, col2, col3, col4, col5 = st.columns(5)
+
+def writetostreamlit(todoitem):
+    col1.subheader(todoitem)
+    collection = db.collection('Appliances').where("type", "==", todoitem).get()
+    for doc in collection:
+        d = doc.to_dict()
+        for entry in d:
+            line = entry + ': ' + d[entry]
+            col1.markdown(line)
+
+for i in items:
+    writetostreamlit(i)
