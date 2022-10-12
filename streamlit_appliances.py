e2ed02818a4b095376d20546da522c05808648de
Victor <vchen2120@gmail.com>
2022-10-12 09:56:38 -0700

    maybe work this time

--- streamlit_appliances.py
+++ streamlit_appliances.py
@@ -4,18 +4,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
-db = firestore.client()
-
-entire_collection = db.collection('Vacancy').get()
-
 if not firebase_admin._apps:
     cred = credentials.Certificate(st.secrets["firestore_keys_baby"])
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
 entire_collection = db.collection('Appliances').get()
+
 for doc in entire_collection:
     d = doc.to_dict()
     for entry in d:
