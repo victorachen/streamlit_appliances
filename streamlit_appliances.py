d13a61c5d83fdef0126b2f668c188e9fb6d8a0b3
Victor <vchen2120@gmail.com>
2022-11-11 08:37:51 -0800

    ...

--- streamlit_appliances.py
+++ streamlit_appliances.py
@@ -21,7 +21,8 @@ from google.oauth2 import service_account
 
 key_dict = json.loads(st.secrets['textkey'])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds)
+firebase_admin.initialize_app(creds)
+# db = firestore.Client(credentials=creds)
 
 st.set_page_config(
     page_title='To Do Items',
